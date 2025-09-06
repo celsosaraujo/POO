19067c479d8e0d09f0c9dba0f1f6d7b4a981817d
celso.saraujo <celso.saraujo@hotmail.com>
2025-09-05 22:11:33 -0300

    Declaração de constutores

---
+++ 02_Construtores/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado = new Quadrado(5);
+            quadrado.ImprimirArea();
+
+            Retangulo obj3 = new Retangulo(15,5);
+            obj3.ImprimeArea();
+
+            Circulo obj4 = new Circulo(7.5);            
+            obj4.ImprimeArea();
+
+            Triangulo obj5 = new Triangulo(12.75,25.5);
+            obj5.ImprimeArea();
+        }      
+
+    }
+
+    public class Quadrado
+    {
+        //Modificar de acesso Private
+        //pode ser acessado apenas dentro da Classe
+        private int Lado;
+
+        //O Construtor serve para instanciar o objeto e
+        //Inicializar os atributos
+        public Quadrado( int Lado)
+        {
+            //this: acessa o membro da classe
+            this.Lado = Lado;
+        }
+       
+        public int CalculaArea()
+        {
+            return Lado * Lado;
+        }
+
+        public void ImprimirArea()
+        {
+            Console.WriteLine($"Quadrado com lado de {Lado} possui uma área de {CalculaArea()}");
+        }
+    }
+
+    public class Retangulo
+    {
+        private int Largura, Altura;
+
+        public Retangulo( int Largura, int Altura)
+        {
+            this.Largura = Largura;
+            this.Altura = Altura;
+        }
+        public int CalculaArea()
+        {
+            return Largura * Altura;
+        }
+
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Retângulo com largura de {Largura}, altura de {Altura} possui uma área de {CalculaArea()}");
+        }
+
+    }
+
+    public class Circulo
+    {
+        private double Raio;
+
+        public Circulo(double Raio)
+        {
+            this.Raio = Raio;
+        }
+
+        public double CalculaArea()
+        {
+            return Math.Pow(Raio, 2) * Math.PI;
+        }
+
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Círculo com raio de {Raio:N2} possui uma área de {CalculaArea():N2}");
+        }
+    }
+
+    public class Triangulo
+    {
+        private double Base, Altura;
+
+        public Triangulo(double Base, double Altura)
+        {
+            this.Base = Base;
+            this.Altura = Altura;
+        }
+
+        public double CalculaArea()
+        {
+            return Base * Altura / 2;
+        }
+
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Triângulo com base de {Base:N2}, altura de {Altura:N2} possui uma área de {CalculaArea():N2}");
+
+        }
+    }
+}
