4ffee89d7a4754a644acb6fbd862566866d244e1
celso.saraujo <celso.saraujo@hotmail.com>
2025-09-19 20:52:55 -0300

    Criação do projeto

---
+++ 10_HerancaBranchGIT/Forma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_HerancaBranchGIT
+{
+    internal abstract class Forma
+    {
+        public abstract string getTipo();
+
+        public abstract double getArea();
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Forma: {getTipo()} com área de: {getArea():(n2)}");
+        }
+
+    }
+}
