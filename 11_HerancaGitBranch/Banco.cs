541da9a46b512a643d7f010a4a05c833f1b73f13
celso.saraujo <celso.saraujo@hotmail.com>
2025-09-22 20:53:09 -0300

    Inicialização do Projeto Banco

---
+++ 11_HerancaGitBranch/Banco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_HerancaGitBranch
+{
+    internal abstract class Banco
+    {
+        public String NomeBanco {  get; set; }
+        abstract public string LinhaDigitavel();
+        public void imprimirDados()
+        {
+            Console.WriteLine($"Banco: {NomeBanco} - Linha Digitável: {LinhaDigitavel()}");
+        }
+
+    }
+}
