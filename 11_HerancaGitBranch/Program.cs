7773cf77fb90749c1c1bf9cb58dedf5ea70cd02c
DerJohnBR <derjohnbr@gmail.com>
2025-09-22 21:04:57 -0300

    Conclusão do Banco Original.

--- 11_HerancaGitBranch/Program.cs
+++ 11_HerancaGitBranch/Program.cs
@@ -12,6 +12,9 @@ namespace _11_HerancaGitBranch
         {
             Bradesco bancoBradesco = new Bradesco { NomeBanco = "Banco Bradesco S.A." };
             bancoBradesco.imprimirDados();
+
+            Original bancoOriginal = new Original { NomeBanco = "Banco Original S.A." };
+            bancoOriginal.imprimirDados();
         }
     }
 }
