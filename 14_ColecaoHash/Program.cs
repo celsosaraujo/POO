260ba7e7a3f4be25241921dbe8aba8113644943c
celso.saraujo <celso.saraujo@hotmail.com>
2025-09-29 19:34:37 -0300

    Correção dos exercícios

---
+++ 14_ColecaoHash/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_ColecaoHash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            int valor;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"Informe o {i}º valor: "  );
+                valor = int.Parse( Console.ReadLine() );
+                set.Add( valor );
+
+            }
+            foreach (int i in set)
+            {
+                Console.WriteLine($"Número: {i}" );
+            }
+        }
+    }
+}
---
+++ 15_ColecaoDicionario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ColecaoDicionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ra;
+            string nome;
+
+            Dictionary<int, string> dados = new Dictionary<int, string>(); 
+
+            while (true)
+            {
+                Console.Write("Informe o RA: ");
+                ra = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o Nome: ");
+                nome = Console.ReadLine();
+
+                dados.Add(ra, nome);
+
+                Console.Write("Deseja incluir outro aluno? (S/N) ");
+                if (Console.ReadLine().ToUpper() == "N")
+                    break;
+
+            }
+
+            foreach (var item in dados)
+            {
+                Console.WriteLine( $"RA: {item.Key} Nome: {item.Value}");
+            }
+
+        }
+    }
+}
